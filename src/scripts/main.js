2095d6ffe45d4bfa520b5ad9560b09950afcd74a
kpoden <67436313+kpoden@users.noreply.github.com>
2021-05-20 15:36:42 +0500

    dog added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,11 +6,9 @@ const tl = gsap.timeline({
 })
 
 
-tl.to('.color1', 0.4, { width: '100%', ease: "power3.inOut"})
-tl.to('.color2', 0.6, { width: '100%'})
-tl.from('.header__title', 0.5, { y: '-500'})
-tl.from('.header__subtitle',  { opacity: '0'})
-tl.to('.header__title', { opacity: '1'})
+tl.to('.color1', 0.3, { width: '100%', ease: "Expo.easeInOut"})
+tl.to('.color2', 0.5, { width: '100%'})
+tl.to('.header__title', 0.4, { y: '0', ease: "Expo.easeInOut"})
 tl.to('.header__subtitle',  { opacity: '1'})
 
 
@@ -29,6 +27,22 @@ gsap.to(".skill__el", {
     scrollTrigger: ".skill__el", 
     y: '0', 
     opacity: '1' });
+
+gsap.to(".header__image", {
+    scrollTrigger: ".header__image", 
+    scaleX: '1',
+    transformOrigin: 'left center',
+    delay: .8});
+
+
+
+
+gsap.to(".header-img-wrap", 1.5,{
+    scrollTrigger: ".header__image", 
+    x: '-500', 
+    ease: "Expo.easeInOut",
+    delay: 1
+    });
 
 
 
